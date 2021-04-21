96cf898ba11d0d42439ead9940dcd3996609d40f
Amal George Thomas <amal.george@chargebee.com>
2021-04-21 18:22:39 +0530

    Variant switching

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
 
     $('.color-choose input').on('click', function () {
-        var headphonesColor = $(this).attr('data-image');
-
-        $('.active').removeClass('active');
-        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
-        $(this).addClass('active');
+        const modelViewer = document.querySelector('#model-demo');
+        modelViewer.variantName = $(this).attr('data-image');
     });
 });
