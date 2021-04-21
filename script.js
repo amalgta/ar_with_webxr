682cb5a2f4f80ce8dd90c4e9fcfd27af671f70a1
Amal George Thomas <amalgta@gmail.com>
2021-04-21 21:48:26 +0530

    Testing AR buttons

--- script.js
+++ script.js
@@ -15,7 +15,20 @@ $(document).ready(function () {
     const modelPath = `https://ar-with-webxr.s3.us-east-2.amazonaws.com/assets/3d/${model}.glb`
     const modelViewer = document.querySelector('#model-demo');
     modelViewer.src = modelPath;
+    modelViewer.addEventListener('load', () => {
+        const center = modelViewer.getCameraTarget();
+        const size = modelViewer.getDimensions();
+        const x2 = size.x / 2;
+        const y2 = size.y / 2;
+        const z2 = size.z / 2;
 
+        modelViewer.updateHotspot({
+            name: 'hotspot-dim+X-Y',
+            position: `${center.x + x2} ${center.y + y2} ${center.z}`
+        });
+        modelViewer.querySelector('button[slot="hotspot-dim+X-Y"]').textContent =
+            `An AR button`;
+    });
     $('.color-choose input').on('click', function () {
         modelViewer.variantName = $(this).attr('data-image');
     });
