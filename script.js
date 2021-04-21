b0d269e6f1ea9d614e24fb4a057b0de569968d57
Amal George Thomas <amal.george@chargebee.com>
2021-04-21 18:38:04 +0530

    Externalize model loading

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 $(document).ready(function () {
+    let model = getParameterByName("model");
+    if (!model) {
+        model = "chair-variant";
+    }
+    const modelPath = `https://ar-with-webxr.s3.us-east-2.amazonaws.com/assets/3d/${model}.glb`
+    const modelViewer = document.querySelector('#model-demo');
+    modelViewer.src = modelPath;
 
     $('.color-choose input').on('click', function () {
-        const modelViewer = document.querySelector('#model-demo');
         modelViewer.variantName = $(this).attr('data-image');
     });
 });
