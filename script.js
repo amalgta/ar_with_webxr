5e1d2d79393573074a04aadc931cfab968ec823f
Amal George Thomas <amalgta@gmail.com>
2021-04-21 15:36:23 +0530

    Testing model viewer

--- script.js
+++ script.js
@@ -8,20 +8,4 @@ $(document).ready(function () {
         $(this).addClass('active');
     });
 
-    const modelViewer = document.querySelector("model-viewer");
-    window.switchSrc = (element, name) => {
-        const base = "../../assets/ShopifyModels/" + name;
-        modelViewer.src = base + '.glb';
-        modelViewer.poster = base + '.png';
-        const slides = document.querySelectorAll(".slide");
-        slides.forEach((element) => {
-            element.classList.remove("selected");
-        });
-        element.classList.add("selected");
-    };
-
-    document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
-        // Keep slider interactions from affecting the XR scene.
-        ev.preventDefault();
-    });
 });
