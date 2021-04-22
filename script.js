195bfb454a0cebe3ef4bba4d933395b7e9f5d9a5
Amal George Thomas <amalgta@gmail.com>
2021-04-22 17:10:59 +0530

    Testing AR buttons

--- script.js
+++ script.js
@@ -8,26 +8,52 @@ function getParameterByName(name, url = window.location.href) {
 }
 
 $(document).ready(function () {
+    const modelViewer = document.querySelector('#model-demo');
+    const buttonTwo = document.querySelector('#button_two');
+    const buttonOne = document.querySelector('#button_one');
+    modelViewer.addEventListener('play', () => {
+        setTimeout(function () {
+            modelViewer.pause();
+        }, modelViewer.duration * 1000);
+    });
+    buttonTwo.addEventListener('click', () => {
+            if (modelViewer.availableAnimations.includes("SPLIT")) {
+                modelViewer.animation = (modelViewer.availableAnimations.find((item) => {
+                    return item === "SPLIT"
+                }));
+                modelViewer.play();
+            }
+        }
+    );
+    buttonOne.addEventListener('click', () => {
+        modelViewer.pause();
+        modelViewer.currentTime = 0;
+        }
+    );
     let model = getParameterByName("model");
     if (!model) {
         model = "chair-variant";
     }
+    //const modelPath = `../assets/glb/${model}.glb`;
     const modelPath = `https://ar-with-webxr.s3.us-east-2.amazonaws.com/assets/3d/${model}.glb`
-    const modelViewer = document.querySelector('#model-demo');
     modelViewer.src = modelPath;
+
     modelViewer.addEventListener('load', () => {
         const center = modelViewer.getCameraTarget();
         const size = modelViewer.getDimensions();
         const x2 = size.x / 2;
         const y2 = size.y / 2;
         const z2 = size.z / 2;
-
         modelViewer.updateHotspot({
             name: 'hotspot-dim+X-Y',
             position: `${center.x + x2} ${center.y + y2} ${center.z}`
         });
         modelViewer.querySelector('button[slot="hotspot-dim+X-Y"]').textContent =
             `An AR button`;
+        // if (modelViewer.availableAnimations) {
+        //     modelViewer.animation = (modelViewer.availableAnimations[0]);
+        //     modelViewer.play();
+        // }
     });
     $('.color-choose input').on('click', function () {
         modelViewer.variantName = $(this).attr('data-image');
